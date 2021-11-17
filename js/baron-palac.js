0b7000c4bb4f99d2b697d7b8cc83cee8634241b9
abdulrahmanmokhlef <abdulrahman.mokhlef@gmail.com.com>
2021-11-17 02:27:06 +0200

    ca

---
+++ js/baron-palac.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+ 
+
+  $('.owl-carousel').owlCarousel({
+        margin: 10,
+        loop:true,
+        nav: false,
+        rtl:true,
+        responsive:{
+            0:{
+                items:1
+            }
+        }
+    })
+    
+});
--- js/main.js
+++ js/main.js
@@ -1,39 +1,25 @@
 $(document).ready(function() {
  
+ 
     // $('.owl-carousel').owlCarousel({
+    //     margin: 10,
     //     loop:true,
-    //     margin:10,
-    //     nav:true,
+    //     nav: true,
+    //     rtl:true,
     //     dots:false,
     //     navText : ['<i class="fa fa-angle-left" aria-hidden="true"></i>','<i class="fa fa-angle-right" aria-hidden="true"></i>'],
     //     responsive:{
     //         0:{
     //             items:1
+    //         },
+    //         600:{
+    //             items:4
+    //         },
+    //         1000:{
+    //             items:4
     //         }
     //     }
-
     // })
-
-    //مواقع اثرية
-    $('.owl-carousel').owlCarousel({
-        margin: 10,
-        loop:true,
-        nav: true,
-        rtl:true,
-        dots:false,
-        navText : ['<i class="fa fa-angle-left" aria-hidden="true"></i>','<i class="fa fa-angle-right" aria-hidden="true"></i>'],
-        responsive:{
-            0:{
-                items:1
-            },
-            600:{
-                items:4
-            },
-            1000:{
-                items:4
-            }
-        }
-    })
 
 
     //avtive tab buy tab id
