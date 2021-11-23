91ef4e5826b3930041820ec39f67d7b1e0912855
abdulrahmanmokhlef <abdulrahman.mokhlef@gmail.com.com>
2021-11-23 19:28:37 +0200

    d

---
+++ js/login.js
@@ -0,0 +1,77 @@
+$(document).ready(function() {
+ 
+    var modal = `<!-- Login Modal -->
+        <div class="modal fade" id="loginModal" tabindex="-1" role="dialog" aria-labelledby="loginModal" aria-hidden="false">
+          <div class="modal-dialog modal-dialog-centered" role="document">
+            
+            <div class="modal-content">
+              
+              <!-- partial:index.partial.html -->
+              <div class="login-form">
+                <form>
+                  <h2>تسجيل الدخول</h2>
+                  <div class="content">
+                    <div class="input-field">
+                      <input type="email" placeholder="البريد الالكتروني أو رقم التليفون"  >
+                    </div>
+                    <div class="input-field">
+                      <input type="password" placeholder="كلمة السر"  >
+                    </div>
+                    
+
+                    <div class="form-check">
+                      <label>تذكرني</label>
+                      <label>
+                        <input type="checkbox" name="checkbox" value="css" />
+                        <span></span>
+                      </label>
+                    </div>
+
+
+                  </div>
+                  <div class="action">
+                    <a  id="createAccount" >إنشاء حساب</a>
+                    <a  id="login">دخول</a>
+                  </div>
+
+                  <div class="forget">
+                    <a href="#" class="link">هل نسيت كلمة السر؟</a>
+                  </div>
+                  
+                </form>
+              </div>
+              <!-- partial -->
+              
+            </div>
+            
+          </div>
+        </div>
+        <!-- Login Modal -->
+  `
+
+  $('.main').append(modal);
+
+  
+    //create account
+    document.getElementById("createAccount").onclick = function () {
+      debugger
+      var url = document.URL;
+      if(url.includes('pages')){
+        location.href = '../pages/create-account.html'; 
+      }else{
+        location.href = './pages/create-account.html'; 
+      }
+    };
+
+    //create account
+    document.getElementById("login").onclick = function () {
+      debugger
+      var url = document.URL;
+      if(url.includes('pages')){
+        location.href = '../pages/profile.html'; 
+      }else{
+        location.href = './pages/profile.html'; 
+      }
+    };
+  
+  });
--- js/main.js
+++ js/main.js
@@ -1,30 +1,11 @@
 $(document).ready(function() {
  
- 
-    // $('.owl-carousel').owlCarousel({
-    //     margin: 10,
-    //     loop:true,
-    //     nav: true,
-    //     rtl:true,
-    //     dots:false,
-    //     navText : ['<i class="fa fa-angle-left" aria-hidden="true"></i>','<i class="fa fa-angle-right" aria-hidden="true"></i>'],
-    //     responsive:{
-    //         0:{
-    //             items:1
-    //         },
-    //         600:{
-    //             items:4
-    //         },
-    //         1000:{
-    //             items:4
-    //         }
-    //     }
-    // })
 
-
+    
     //avtive tab buy tab id
     //navTabClass ex: nav-tab
     activeTab = function(navTabClass,tab){
+        
         debugger
         $( '.' + navTabClass + ' ' +'a[href="#' + tab + '"]').tab('show');
     };
@@ -37,14 +18,14 @@ $(document).ready(function() {
 
     //todo this block of code is temporary commented
     //this to keep active tab opened after reload 
-    // $('a[data-toggle="tab"]').on('show.bs.tab', function(e) {
-    //     localStorage.setItem('activeTab', $(e.target).attr('href'));
-    // });
-    // var activeTab = localStorage.getItem('activeTab');
-    // if(activeTab){
-    //     $('#myTab a[href="' + activeTab + '"]').tab('show');
-    //     $('#myTab2 a[href="' + activeTab + '"]').tab('show');
-    // }
+    $('a[data-toggle="tab"]').on('show.bs.tab', function(e) {
+        localStorage.setItem('activeTab', $(e.target).attr('href'));
+    });
+    var activeTab = localStorage.getItem('activeTab');
+    if(activeTab){
+        $('#myTab a[href="' + activeTab + '"]').tab('show');
+        $('#myTab2 a[href="' + activeTab + '"]').tab('show');
+    }
 
 
     //input file
